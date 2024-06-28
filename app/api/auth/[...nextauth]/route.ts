43cf3482c979b5a158e2feacc163fa634bb8fc53
Erick Lima <ericklima63@gmail.com>
2024-06-28 13:58:43 -0300

    fix: Import Providers from Next Auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,5 @@
 import NextAuth from "next-auth"
-import GitHubProvider from "next-auth/providers/github"
-
-export const authOptions = {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID ?? "",
-            clientSecret: process.env.GITHUB_SECRET ?? "",
-        })
-    ]
-}
+import authOptions from '@/app/lib/configs/auth/authOptions'
 
 export const handler = NextAuth(authOptions)
 
---
+++ app/lib/configs/auth/authOptions.ts
@@ -0,0 +1,12 @@
+import GitHubProvider from "next-auth/providers/github"
+
+const authOptions = {
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID ?? "",
+            clientSecret: process.env.GITHUB_SECRET ?? "",
+        })
+    ]
+}
+
+export default authOptions
